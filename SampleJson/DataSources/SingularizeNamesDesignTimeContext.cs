f48e7b96921318d8584a682cd5156600f7e67383
brgrz <marko@centiva.si>
2019-11-11 12:15:09 +0100

    northwind context

---
+++ SampleJson/DataSources/SingularizeNamesDesignTimeContext.cs
@@ -0,0 +1,26 @@
+﻿using Inflector;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+using System.Globalization;
+
+public class CustomPluralizer : IPluralizer
+{
+	private readonly Inflector.Inflector inflector = new Inflector.Inflector(new CultureInfo("en"));
+
+	public string Pluralize(string name)
+	{
+		return inflector.Pluralize(name) ?? name;
+	}
+	public string Singularize(string name)
+	{
+		return inflector.Singularize(name) ?? name;
+	}
+}
+
+public class CustomDesignTimeServices : IDesignTimeServices
+{
+	public void ConfigureDesignTimeServices(IServiceCollection services)
+	{
+		services.AddSingleton<IPluralizer, CustomPluralizer>();
+	}
+}
