ae13b5f75a5c6f55d69046dc1f9b2e8f1ee0b871
brgrz <marko@centiva.si>
2019-11-11 12:57:08 +0100

    nswag, dbcontext di setup, ..

--- SampleJson/Startup.cs
+++ SampleJson/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using SampleJson.DataSources.Northwind.Models;
 
 namespace SampleJson
 {
@@ -25,7 +27,31 @@ namespace SampleJson
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddRouting(options => options.LowercaseUrls = true);
+			services.AddRouting(options => options.LowercaseQueryStrings = true);
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+			services.AddDbContext<NorthwindDbContext>(options =>
+		options.UseSqlServer(Configuration.GetConnectionString("northwind")));
+
+			// Register the Swagger services
+			services.AddSwaggerDocument(config =>
+			{
+				config.PostProcess = document =>
+				{
+					document.Info.Version = "v1";
+					document.Info.Title = "SampleJson API";
+					document.Info.Description = "Providing sample JSON data";
+					document.Info.TermsOfService = "MIT license";
+					document.Info.Contact = new NSwag.OpenApiContact
+					{
+						Name = "brgrz",
+						Email = string.Empty,
+						Url = "https://twitter.com/brgrz"
+					};
+				};
+			});
+
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +69,11 @@ namespace SampleJson
 
 			app.UseHttpsRedirection();
 			app.UseMvc();
+
+			// Register the Swagger generator and the Swagger UI middlewares
+			app.UseOpenApi();
+			app.UseSwaggerUi3();
+
 		}
 	}
 }
